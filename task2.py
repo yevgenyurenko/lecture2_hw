5a36a53b8c28e32d0c8c0e3ab63f7d0a9db75988
yevgenyurenko <yevgen.yurenko@gmail.com>
2023-06-30 15:26:07 +0200

    lecture2_homework

---
+++ task2.py
@@ -0,0 +1,2 @@
+print("I am  learning Python", "Today is a good day to start", sep = '!!', end = '\n') # "I am  learning Python!!Today is a good day to start"
+print('Python is a beautiful programming language', "I like it", sep = "---", end = '\t') # Python is a beautiful programming language---I like it
